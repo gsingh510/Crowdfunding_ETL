e2e44a2ac11fcd0307d7ab32b5e2bbded521ce20
astorton <astorton760@gmail.com>
2023-05-27 12:46:09 -0700

    Add files via upload

---
+++ Project_2_SGagan V2/crowdfunding_db_select_statements.sql
@@ -0,0 +1,13 @@
+--highlight each select statement to query all data from their respective table
+
+Select * 
+from campaign;
+
+Select * 
+from contact;
+
+Select * 
+from category;
+
+Select * 
+from subcategory;
